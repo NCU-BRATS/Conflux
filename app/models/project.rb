68729bb3d72f57734eaf3c366004521e8c9e4777
Rueian Oneecom <rueiancsie@gmail.com>
2014-12-07 23:25:35 +0800

    setup project friendly_id and association cascade

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,18 @@
 class Project < ActiveRecord::Base
-  has_many :project_participations
+  include FriendlyId
+  friendly_id :name, use: :slugged
+
+  has_many :project_participations, dependent: :destroy
   has_many :users, through: :project_participations
 
-  has_many :issues
-  has_many :sprints
-  has_many :repositories
+  has_many :issues,       dependent: :destroy
+  has_many :sprints,      dependent: :destroy
+  has_many :repositories, dependent: :destroy
 
   validates :name, presence: true
+  validates :name, uniqueness: true
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 end
