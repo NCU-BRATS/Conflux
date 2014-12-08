ababf1dd952506f881f816991ac2f2759e31fdd7
Rueian Oneecom <rueiancsie@gmail.com>
2014-12-08 23:08:47 +0800

    add ProjectParticipationPolicy

--- app/controllers/projects/members_controller.rb
+++ app/controllers/projects/members_controller.rb
@@ -7,17 +7,20 @@ class Projects::MembersController < Projects::ApplicationController
 
   def new
     @participation = @project.project_participations.build
+    authorize @participation
     respond_with @participation
   end
 
   def create
     @participation = @project.project_participations.build(participation_params)
+    authorize @participation
     flash[:notice] = "已成功創建#{ProjectParticipation.model_name.human}" if @participation.save
     respond_with @participation, location: project_members_path
   end
 
   def destroy
     @participation = @project.project_participations.find(params[:id])
+    authorize @participation
     @participation.destroy
     flash[:notice] = "已成功刪除#{ProjectParticipation.model_name.human}"
     respond_with @participation, location: project_members_path
