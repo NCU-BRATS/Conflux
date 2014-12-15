d6e2bd1d3a138da0acb26ce628281d2a5e94a5ca
jasonChiu <jason1122g@gmail.com>
2014-12-16 00:57:19 +0800

    add basic issue CRUD

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  validates :content, :user, :commentable, presence: true
+  validates :content, :user, presence: true
 end
