c81f5ac38d52fe33ee6b48f99f0bfd34165dfc12
Rueian Oneecom <rueiancsie@gmail.com>
2014-12-24 22:19:56 +0800

    parse comment content before saving

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,14 @@
 class Comment < ActiveRecord::Base
+  include ParserConcern
+
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
   validates :content, :user, presence: true
+
+  before_save :parse_content, if: :content_changed?
+
+  def parse_content
+    self.html = self.class.parse content
+  end
 end
