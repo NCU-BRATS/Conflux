ffc371a720d719d5098d5f0a31ca645a96f90b02
Rueian Oneecom <rueiancsie@gmail.com>
2014-12-26 21:39:08 +0800

    add friend_id to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include FriendlyId
+  friendly_id :name, use: :slugged
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
