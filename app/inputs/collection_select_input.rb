bde07d1534a9aaf1501d2252f78d185fbc8ed0fd
Rueian Oneecom <rueiancsie@gmail.com>
2014-12-26 23:54:30 +0800

    add data attrs for each option to simple_form CollectionSelectInput

--- app/inputs/collection_select_input.rb
+++ app/inputs/collection_select_input.rb
@@ -1,5 +1,18 @@
 class CollectionSelectInput < SimpleForm::Inputs::CollectionSelectInput
   def input_html_options
-    super.reverse_merge({:data => {:toggle => 'selectize'}})
+    {:data => { :toggle => 'selectize' }}.deep_merge(super)
+  end
+
+  def input(wrapper_options = nil)
+    return super if input_html_options[:data]['resource-path'].blank?
+    label_method, value_method = detect_collection_methods
+
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+    @builder.select(
+      attribute_name,
+      collection.map {|e| [e.send(label_method), e.send(value_method), { data: {data: e.to_json} } ]},
+      input_options, merged_input_options
+    )
   end
 end
