e1d5787c77930e9cbc9531e7daee9ddc26a1852d
Rueian Oneecom <rueiancsie@gmail.com>
2015-01-07 00:31:11 +0800

    force simple_form to set datetime input as text

--- app/inputs/date_time_input.rb
+++ app/inputs/date_time_input.rb
@@ -1,6 +1,6 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
   def input_html_options
-    {:data => {:toggle => 'datetime-picker'}}.deep_merge(super)
+    {:data => {:toggle => 'datetime-picker'}, :type => :text}.deep_merge(super)
   end
 
   def use_html5_inputs?
