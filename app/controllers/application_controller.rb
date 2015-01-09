b2760427c98d931d667e60e0a7bf07746c197e27
Rueian Oneecom <rueiancsie@gmail.com>
2015-01-09 18:31:13 +0800

    apply JsDefaultResponder globally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
-  responders :flash, :http_cache
+  responders :flash, :http_cache, :js_default
   respond_to :html, :json, :js
 
   # Prevent CSRF attacks by raising an exception.
