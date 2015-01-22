a25ec4d6d4a48c1999bbbb3e32af033a893e294a
Rueian Oneecom <rueiancsie@gmail.com>
2015-01-22 22:41:17 +0800

    fix duplicated project assigment

--- app/controllers/projects/dashboards_controller.rb
+++ app/controllers/projects/dashboards_controller.rb
@@ -1,6 +1,5 @@
 class Projects::DashboardsController < Projects::ApplicationController
   def show
-    @project = Project.friendly.find(params[:project_id])
     @issues = {
       assigned_to_me: @project.issues.includes(:user, :assignee).open.where(assignee: current_user).first(5),
       opened_by_me:   @project.issues.includes(:user, :assignee).open.where(user: current_user).first(5),
