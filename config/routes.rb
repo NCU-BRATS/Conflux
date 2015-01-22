49c72ba72c3eedaaf875c62ecdba69036b8af4dd
JackSie <jetswayss@gmail.com>
2015-01-22 01:11:22 -0800

    add dashboard into route setting

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
 
   resources :profiles
 
+  resource :dashboard, controller: "dashboard", only: [:show]
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
