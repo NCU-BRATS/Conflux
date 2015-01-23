12d3cfe153ff842b3f2d5dd0a28538783be1f400
Rueian Oneecom <rueiancsie@gmail.com>
2015-01-24 00:33:19 +0800

    change root path to dashboard#show

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'dashboard#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
