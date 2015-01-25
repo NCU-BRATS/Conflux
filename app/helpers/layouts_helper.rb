c78a74aab72cbe4d4b9a562ed87eb5e9afd145fd
jasonChiu <jason1122g@gmail.com>
2015-01-25 13:33:19 +0800

    reformat helpers

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,8 +1,10 @@
 # http://blog.55minutes.com/2014/02/easier-nested-layouts-in-rails-34/
 module LayoutsHelper
+
   def parent_layout(layout)
     @view_flow.set(:layout, output_buffer)
     output = render(:file => "layouts/#{layout}")
     self.output_buffer = ActionView::OutputBuffer.new(output)
   end
+
 end
