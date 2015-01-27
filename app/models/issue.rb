866baf3385b38ef4a7d7ec0f1bacaa531fdbd79d
jasonChiu <jason1122g@gmail.com>
2015-01-27 13:35:50 +0800

    refactor issue with labelable concern

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ActiveRecord::Base
   include AASM
   include ParserConcern
   include CommentableConcern
+  include LabelableConcern
 
   sync :all
 
@@ -9,8 +10,6 @@ class Issue < ActiveRecord::Base
   belongs_to :user
   belongs_to :sprint
   belongs_to :assignee, class_name: 'User'
-  has_many :label_links, as: :target, dependent: :destroy
-  has_many :labels, through: :label_links
 
   acts_as_sequenced scope: :project_id
 
@@ -39,22 +38,6 @@ class Issue < ActiveRecord::Base
 
   def self.commentable_find_key
     :sequential_id
-  end
-
-  def label_names
-    labels.order('title ASC').pluck(:title)
-  end
-
-  def remove_labels
-    labels.delete_all
-  end
-
-  def add_labels_by_names(label_names)
-    label_names.each do |label_name|
-      label = project.labels.create_with(
-          color: Label::DEFAULT_COLOR).find_or_create_by(title: label_name.strip)
-      self.labels << label
-    end
   end
 
   def working?
