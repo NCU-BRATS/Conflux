145e0ad7c7a91889fc0e04c48a3e74270b3e8fa1
Rueian Oneecom <rueiancsie@gmail.com>
2015-02-13 14:12:38 +0800

    preload sti descendants in development mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :preload_sti_descendants, if: "Rails.env.development?"
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :title]
+  end
+
+  def preload_sti_descendants
+    [Attachment::Post, Attachment::Snippet, Attachment::Image, Attachment::Other]
   end
 
 end
