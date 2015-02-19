37b625b5246e53a83e364da2ac5911f3f3a184ee
Rueian Oneecom <rueiancsie@gmail.com>
2015-02-19 18:16:38 +0800

    initialize attachment counter caches in migration

--- db/migrate/20150218133914_add_post_snippet_image_other_count_to_project.rb
+++ db/migrate/20150218133914_add_post_snippet_image_other_count_to_project.rb
@@ -4,5 +4,13 @@ class AddPostSnippetImageOtherCountToProject < ActiveRecord::Migration
     add_column :projects, :snippets_count, :integer, null: false, default: 0
     add_column :projects, :images_count, :integer, null: false, default: 0
     add_column :projects, :other_attachments_count, :integer, null: false, default: 0
+
+    Project.all.each do |p|
+      p.posts_count    = p.posts.count
+      p.snippets_count = p.snippets.count
+      p.images_count   = p.images.count
+      p.other_attachments_count = p.other_attachments.count
+      p.save
+    end
   end
 end
