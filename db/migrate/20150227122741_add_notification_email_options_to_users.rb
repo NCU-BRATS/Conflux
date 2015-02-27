b1b8fc4b2cf5701769c9ae9e6f1aba692e62b772
bdsword <thetruebloodsword@gmail.com>
2015-02-28 01:10:13 +0800

    delete useless code

--- db/migrate/20150227122741_add_notification_email_options_to_users.rb
+++ db/migrate/20150227122741_add_notification_email_options_to_users.rb
@@ -1,5 +1,4 @@
 class AddNotificationEmailOptionsToUsers < ActiveRecord::Migration
-    add_column :users, :mention_email, :boolean, default: true
   def change
     add_column :users, :mention_email, :boolean, default: true
     add_column :users, :participating_email, :boolean, default: true
