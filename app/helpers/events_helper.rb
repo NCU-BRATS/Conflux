6aa4da823868d45dbd1612bde3a712c2f36f0f0d
jasonChiu <jason1122g@gmail.com>
2015-03-02 08:51:40 +0800

    replace double quote with single quote

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,27 +1,27 @@
 module EventsHelper
+
   def event_action_name(event)
     target = if event.target_type
                event.target_type.titleize.downcase
              else
                'project'
              end
-
-    action_name event, I18n.t(["project", target].join(".")+"s")
+    action_name event, I18n.t(['project', target].join('.')+'s')
   end
 
   def action_name (event, target)
     if event.closed?
-      I18n.t("action.target.close" , :target => target)
+      I18n.t('action.target.close', :target => target)
     elsif event.joined?
-      I18n.t("action.target.join" , :target => target)
+      I18n.t('action.target.join', :target => target)
     elsif event.left?
-      I18n.t("action.target.left" , :target => target)
+      I18n.t('action.target.left', :target => target)
     elsif event.uploaded?
-      I18n.t("action.target.upload" , :target => target)
+      I18n.t('action.target.upload', :target => target)
     elsif event.comment?
-      I18n.t("action.target.write" , :target => target)
+      I18n.t('action.target.write', :target => target)
     else
-      I18n.t("action.target.open" , :target => target)
+      I18n.t('action.target.open', :target => target)
     end
   end
 
