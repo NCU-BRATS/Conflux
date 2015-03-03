fbc0f92e108ce0a5ff70e46fa206143d95df02ee
bdsword <thetruebloodsword@gmail.com>
2015-03-03 23:07:12 +0800

    redirect to user's original page instead of dashboard page

--- app/controllers/profiles/registrations_controller.rb
+++ app/controllers/profiles/registrations_controller.rb
@@ -1,7 +1,13 @@
 class Profiles::RegistrationsController < Devise::RegistrationsController
   layout 'profile'
 
+  protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << [:name,:title, :phone, :url, :github, :linkedin, :facebook, :twitter]
+  end
+
+  def after_update_path_for(resource)
+    profile_path
   end
 end
