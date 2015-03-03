d2f21f50df507926eaea8c7474058c294911cbca
bdsword <thetruebloodsword@gmail.com>
2015-03-03 19:25:45 +0800

    add extra fields to users profile edit page

--- app/controllers/profiles/registrations_controller.rb
+++ app/controllers/profiles/registrations_controller.rb
@@ -2,6 +2,6 @@ class Profiles::RegistrationsController < Devise::RegistrationsController
   layout 'profile'
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) << [:name, :title]
+    devise_parameter_sanitizer.for(:account_update) << [:name,:title, :phone, :url, :github, :linkedin, :facebook, :twitter]
   end
 end
