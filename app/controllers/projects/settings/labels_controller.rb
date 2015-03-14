2630ee65a338a25e4c176db28b69fceed925b17c
bdsword <thetruebloodsword@gmail.com>
2015-03-14 12:41:38 +0800

    move members_controller and labels_controller to settings namespace

--- app/controllers/projects/settings/labels_controller.rb
+++ app/controllers/projects/settings/labels_controller.rb
@@ -1,33 +1,41 @@
-class Projects::LabelsController < Projects::ApplicationController
+class Projects::Settings::LabelsController < Projects::SettingsController
 
   def index
     @q = @project.labels.search(params[:q])
     @labels = @q.result.order_by_name.page(params[:page]).per(20)
-    respond_with @project, @labels
+    respond_with @project, :settings, @labels
   end
 
   def new
     @label = @project.labels.new
-    respond_with @project, @label
+    respond_with @project, :settings, @label
   end
 
   def create
     @label = @project.labels.create(label_params)
-    respond_with @project, @label, location: project_labels_path(@project)
+    respond_with @project, :settings, @label, location: project_settings_labels_path
   end
 
   def edit
-    respond_with @project, @label
+    respond_with @project, :settings, @label
   end
 
   def update
     @label.update_attributes(label_params)
-    respond_with @project, @label, location: project_labels_path(@project)
+    respond_with @project, :settings, @label, location: project_settings_labels_path
   end
 
   def destroy
     @label.destroy
-    respond_with @project, @label
+    respond_with @project, :settings, @label
+  end
+
+  def model
+    Label
+  end
+
+  def model_sym
+    :label
   end
 
   protected
