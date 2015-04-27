2a63b85046653fa7816e7e395e70900e5c9be2c1
jasonChiu <jason1122g@gmail.com>
2015-04-27 21:58:10 +0800

    add statistic policy

--- app/policies/statistic_policy.rb
+++ app/policies/statistic_policy.rb
@@ -1,7 +1,15 @@
 class StatisticPolicy < ProjectResourcePolicy
 
   def users?
-    true
+    is_user_project_public? || is_user_project_member?
+  end
+
+  def tasks?
+    is_user_project_public? || is_user_project_member?
+  end
+
+  def attachments?
+    is_user_project_public? || is_user_project_member?
   end
 
 end
