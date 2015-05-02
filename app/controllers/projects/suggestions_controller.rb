b94feff9893da5915b7ec0e1f115e56c372bad99
bdsword <thetruebloodsword@gmail.com>
2015-05-02 11:32:30 +0800

    prevent suggestion current user

--- app/controllers/projects/suggestions_controller.rb
+++ app/controllers/projects/suggestions_controller.rb
@@ -2,7 +2,7 @@ class Projects::SuggestionsController < Projects::ApplicationController
 
   def suggestions
     @issues = @project.issues
-    @members = @project.members
+    @members = @project.members.where.not(id: current_user.id)
   end
 
   def model
