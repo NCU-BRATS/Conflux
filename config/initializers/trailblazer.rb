ea63ea9765d371424e5a480afdda696fc0dbb13f
Rueian Oneecom <rueiancsie@gmail.com>
2015-05-15 16:40:09 +0800

    trailblazer autoloading

---
+++ config/initializers/trailblazer.rb
@@ -0,0 +1,5 @@
+# https://github.com/apotonick/gemgem-trbrb/blob/chapter-5/config/initializers/trailblazer.rb
+
+require 'reform/form/coercion'
+require "trailblazer/autoloading"
+require "trailblazer/operation/controller/active_record"
