f8441f13781ef4cbdf935bc39a06ca2bc516e9f8
Rueian Oneecom <rueiancsie@gmail.com>
2015-05-16 10:02:00 +0800

    refactor poll with reform

--- app/controllers/projects/polls_controller.rb
+++ app/controllers/projects/polls_controller.rb
@@ -13,53 +13,38 @@ class Projects::PollsController < Projects::ApplicationController
   end
 
   def new
-    @poll = @project.polls.build
-    @poll.options.build
-    @poll.comments.build
-    respond_with @project, @poll
+    @form = Poll::Create.new(current_user, @project)
+    respond_with @project, @form
   end
 
   def create
-    @poll = @project.polls.build(poll_params)
-    @poll.user = current_user
-    @poll.comments.each { |comment| comment.user = current_user }
-    if @poll.save
-      event_service.open_poll(@poll, current_user)
-      notice_service.open_poll(@poll, current_user)
-      mention_service.mention_filter(:html, @poll.comments.first) # The first comment is the description of the poll
-    end
-    respond_with @project, @poll
+    @form = Poll::Create.new(current_user, @project)
+    @form.process(params)
+    respond_with @project, @form
   end
 
   def update
-    @poll.update(poll_params)
-    respond_with @project, @poll
+    @form = Poll::Update.new(current_user, @poll)
+    @form.process(params)
+    respond_with @project, @form
   end
 
   def close
-    if @poll.close!
-      event_service.close_poll(@poll, current_user)
-      notice_service.close_poll(@poll, current_user)
-    end
-    respond_with @project, @poll
+    @form = Poll::Close.new(current_user, @poll)
+    @form.process
+    respond_with @project, @form
   end
 
   def reopen
-    if @poll.reopen!
-      event_service.reopen_poll(@poll, current_user)
-      notice_service.reopen_poll(@poll, current_user)
-    end
-    respond_with @project, @poll
+    @form = Poll::Reopen.new(current_user, @poll)
+    @form.process
+    respond_with @project, @form
   end
 
   protected
 
   def resource
     @poll ||= @project.polls.where(:sequential_id => params[:id]).first
-  end
-
-  def poll_params
-    params.require(:poll).permit(:title, :status, :allow_multiple_choice, comments_attributes: [ :content ], options_attributes: [:id, :_destroy, :title] )
   end
 
 end
