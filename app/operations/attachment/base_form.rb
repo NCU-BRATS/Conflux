19e47d6896df19965abf0f3fd9bf14f72d612a9e
jasonChiu <jason1122g@gmail.com>
2015-05-17 13:29:23 +0800

    add reform operations for attachment

---
+++ app/operations/attachment/base_form.rb
@@ -0,0 +1,27 @@
+class Attachment < ActiveRecord::Base
+  class BaseForm < Reform::Form
+
+    model :attachment
+
+    property :name
+    property :path
+    property :path_cache
+
+    validates :name, :path, presence: true
+
+    private
+
+    def event_service
+      EventCreateService.new
+    end
+
+    def notice_service
+      NoticeCreateService.new
+    end
+
+    def mention_service
+      MentionService.new
+    end
+
+  end
+end
