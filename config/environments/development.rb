e39c2050011fc1716626710e44fdbc86ad0a42c7
Rueian Oneecom <rueiancsie@gmail.com>
2015-05-21 10:37:04 +0800

    setup react

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'conflux.dev' }
+
+  config.react.variant = :development
 end
