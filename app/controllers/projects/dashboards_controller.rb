cc5f08c0f35d11fa7439e799b2c617b84a047c5a
Rueian Oneecom <rueiancsie@gmail.com>
2015-05-30 00:03:21 +0800

    rewrite event list in project dashboard with react

--- app/controllers/projects/dashboards_controller.rb
+++ app/controllers/projects/dashboards_controller.rb
@@ -3,7 +3,6 @@ class Projects::DashboardsController < Projects::ApplicationController
   def show
     @issues = issues
     @posts  = @project.posts.includes(:user).latest.first(3)
-    @events = events
     respond_with @project
   end
 
@@ -23,17 +22,6 @@ class Projects::DashboardsController < Projects::ApplicationController
     else # default are issues assigned to me
       @project.issues.includes(:user).open.where(assignee: current_user).order('id DESC').first(5)
     end
-  end
-
-  def events
-    params[:event] ||= 'all'
-    if params[:event] == 'all'
-      @project.events.includes(:author).order('id DESC')
-    elsif params[:event] == 'Attachment'
-      @project.events.includes(:author).order('id DESC').where(target_type: Attachment.subclasses)
-    else
-      @project.events.includes(:author).order('id DESC').where(target_type: params[:event])
-    end.page( params[:page] ).per( params[:per] || 15 )
   end
 
 end
