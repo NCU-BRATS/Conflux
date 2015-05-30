02dd49bb0a462164ac1c6b520b0b9207dd6cd802
bdsword <thetruebloodsword@gmail.com>
2015-05-30 15:33:28 +0800

    poll operation base form are not composite form now

--- app/operations/poll_operation/create.rb
+++ app/operations/poll_operation/create.rb
@@ -1,28 +1,23 @@
 module PollOperation
   class Create < BaseForm
-    property :content, on: :comment
+    property :content, virtual: true
     validates :content, presence: true
 
     def initialize(current_user, project)
       @current_user = current_user
       @project      = project
       poll          = Poll.new(options: [PollingOption.new])
-      comment       = Comment.new
-      super({poll: poll, comment: comment})
+      super(poll)
     end
 
     def process(params)
-      poll, comment = @model[:poll], @model[:comment]
       save do |hash|
-        comment.user = @current_user
-        poll.user    = @current_user
-        poll.project = @project
-        poll.comments << comment
-        poll.options_attributes = hash[:poll][:options]
-        if poll.save
-          ParticipationOperation::Create.new(@current_user, poll).process
-          BroadcastService.fire(:on_poll_created, poll, @current_user)
-          mention_service.mention_filter(:html, comment)
+        @model.user    = @current_user
+        @model.project = @project
+        @model.options_attributes = hash[:options]
+        if @model.save
+          ParticipationOperation::Create.new(@current_user, @model).process
+          BroadcastService.fire(:on_poll_created, @model, @current_user)
         end
       end if validate(params[:poll]) && sync
     end
