c0bd419c6d2d88dccb60ce5c764778aa6977f1db
bdsword <thetruebloodsword@gmail.com>
2015-05-30 15:33:28 +0800

    poll operation base form are not composite form now

--- app/operations/poll_operation/base_form.rb
+++ app/operations/poll_operation/base_form.rb
@@ -5,14 +5,12 @@ module PollOperation
       def_delegator :Poll, :reflect_on_association
     end
 
-    include Composition
-
     model :poll
 
-    property :title,                 on: :poll
-    property :status,                on: :poll
-    property :allow_multiple_choice, on: :poll
-    collection :options,             on: :poll, populate_if_empty: PollingOption do
+    property :title
+    property :status
+    property :allow_multiple_choice
+    collection :options, populate_if_empty: PollingOption do
       property :id
       property :title
       property :_destroy, virtual: true
