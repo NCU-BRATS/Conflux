e6acb8e95f7c53d853e291faf0be620e82a6a53b
Rueian Oneecom <rueiancsie@gmail.com>
2015-06-02 02:01:31 +0800

    remove depandnecy of act_as_vote of PollingOption

--- app/models/polling_option.rb
+++ app/models/polling_option.rb
@@ -1,14 +1,25 @@
 class PollingOption < ActiveRecord::Base
-  acts_as_votable
 
   belongs_to :poll
 
   validates :title, presence: true
 
-  update_index('projects#poll') { poll if should_reindex? }
+  update_index('projects#poll') { poll }
 
-  def should_reindex?
-    destroyed? || (previous_changes.keys & ['title', 'cached_votes_total']).present?
+  def votes_total
+    self.voted_users.size
+  end
+
+  def voted_by?(user)
+    self.voted_users.any? {|u| u['id'] == user.id}
+  end
+
+  def voted_by(user)
+    self.voted_users << user
+  end
+
+  def unvoted_by(user)
+    self.voted_users.reject! {|u| u['id'] == user.id}
   end
 
 end
