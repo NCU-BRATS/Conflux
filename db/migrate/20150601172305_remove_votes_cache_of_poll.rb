1841152d3b169b53709330612d845bce2732e735
Rueian Oneecom <rueiancsie@gmail.com>
2015-06-02 16:04:53 +0800

    cached user likes on likeable

--- db/migrate/20150601172305_remove_votes_cache_of_poll.rb
+++ db/migrate/20150601172305_remove_votes_cache_of_poll.rb
@@ -7,6 +7,6 @@ class RemoveVotesCacheOfPoll < ActiveRecord::Migration
     remove_column :polling_options, :cached_weighted_score,   :integer, index: true, default: 0
     remove_column :polling_options, :cached_weighted_total,   :integer, index: true, default: 0
     remove_column :polling_options, :cached_weighted_average, :float,   index: true, default: 0.0
-    add_column    :polling_options, :voted_users,  :json, default: []
+    add_column    :polling_options, :voted_users,  :jsonb, default: []
   end
 end
