bc39fda91cea5807c231e4bb3ee485e3a0e18e7e
jasonChiu <jason1122g@gmail.com>
2015-06-02 22:52:02 +0800

    add private_pub to issue controller

--- app/controllers/projects/issues_controller.rb
+++ app/controllers/projects/issues_controller.rb
@@ -9,6 +9,8 @@ class Projects::IssuesController < Projects::ApplicationController
   end
 
   def show
+    @private_pub_channel1 = "/projects/#{@project.id}/issues/#{@issue.sequential_id}"
+    @private_pub_channel2 = "/issue/#{@issue.id}/comments"
     respond_with @project, @issue
   end
 
@@ -20,31 +22,59 @@ class Projects::IssuesController < Projects::ApplicationController
   def create
     @form = IssueOperation::Create.new(current_user, @project)
     @form.process(params)
+    PrivatePub.publish_to( private_pub_channel, {
+         action: 'create',
+         target: 'issue',
+         data:   private_pub_data
+     })
     respond_with @project, @form
   end
 
   def update
     @form = IssueOperation::Update.new(current_user, @project, @issue)
     @form.process(params)
+    PrivatePub.publish_to( private_pub_channel, {
+         action: 'update',
+         target: 'issue',
+         data:   private_pub_data
+     })
     respond_with @project, @form
   end
 
   def close
     @form = IssueOperation::Close.new(current_user, @project, @issue)
     @form.process
+    PrivatePub.publish_to( private_pub_channel, {
+         action: 'update',
+         target: 'issue',
+         data:   private_pub_data
+     })
     respond_with @project, @form
   end
 
   def reopen
     @form = IssueOperation::Reopen.new(current_user, @project, @issue)
     @form.process
+    PrivatePub.publish_to( private_pub_channel, {
+         action: 'update',
+         target: 'issue',
+         data:   private_pub_data
+     })
     respond_with @project, @form
   end
 
   protected
 
   def resource
-    @issue ||= @project.issues.where( :sequential_id => params[:id] ).first
+    @issue ||= @project.issues.includes( participations: :user ).where( :sequential_id => params[:id] ).first
+  end
+
+  def private_pub_channel
+    @private_pub_channel ||= "/projects/#{@project.id}/issues/#{@form.model.sequential_id}"
+  end
+
+  def private_pub_data
+    @form.model.as_json(include: [ :user, :sprint, :assignee, :labels, :participations => { include: [ :user ] } ])
   end
 
 end
