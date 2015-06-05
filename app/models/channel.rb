e1129177731d7cfc3ec9283a603654030610f9a7
jasonChiu <jason1122g@gmail.com>
2015-06-05 23:21:40 +0800

    fix digital name of models with friendly id

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -12,6 +12,7 @@ class Channel < ActiveRecord::Base
   has_many :messages, dependent: :destroy
 
   validates :name, :project, presence: true
+  validates :name, format: { with: /\A[a-zA-Z][a-zA-Z0-9_]+\Z/, message: I18n.t('validation.slug.format') }
 
   before_save :parse_announcement, if: :announcement_changed?
 
