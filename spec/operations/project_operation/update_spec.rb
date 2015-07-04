40f651d7f4e60e1372eaab78cbe723f020dfe48c
JackSie <jetswayss@gmail.com>
2015-07-04 05:58:35 -0700

    add project's operation update spec

---
+++ spec/operations/project_operation/update_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ProjectOperation::Update do
+  include_context 'project with members'
+
+  subject { ProjectOperation::Update.new(@members[0], @project) }
+
+  describe '#process' do
+    context 'when given valid params' do
+
+      it 'change the name' do
+        subject.process(new_param({ project: {name: 'NewName'} }))
+        expect(@project.name).to eq 'NewName'
+      end
+
+    end
+
+    context 'when given params without project' do
+      it 'raise ActionController::ParameterMissing' do
+        expect{subject.process(new_param)}.to raise_error ActionController::ParameterMissing
+      end
+    end
+  end
+end
