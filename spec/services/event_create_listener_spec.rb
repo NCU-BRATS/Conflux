7f03b123e15359f804130a498ad0785b32878dd6
JackSie <jetswayss@gmail.com>
2015-07-04 07:55:48 -0700

    add issue related event_create_listener's spce

---
+++ spec/services/event_create_listener_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe EventCreateListener do
+
+  include_context 'issue sprint with project members and labels'
+
+  describe '#process' do
+    context 'when given valid params' do
+      before(:context) do
+
+      end
+
+      it 'create a event when a issue is created' do
+        event = EventCreateListener.on_issue_created(@issue,@members[0])
+        condiction = [
+            event.project == @issue.project,
+            event.target_id == @issue.id,
+            event.target_type == @issue.class.name,
+            event.author_id == @members[0].id,
+            event.created? == true
+        ]
+        expect(condiction).to all( be true )
+      end
+
+      it 'create a event when a issue is closed' do
+        event = EventCreateListener.on_issue_closed(@issue,@members[0])
+        condiction = [
+            event.project == @issue.project,
+            event.target_id == @issue.id,
+            event.target_type == @issue.class.name,
+            event.author_id == @members[0].id,
+            event.closed? == true
+        ]
+        expect(condiction).to all( be true )
+      end
+
+      it 'create a event when a issue is reopened' do
+        event = EventCreateListener.on_issue_reopened(@issue,@members[0])
+        condiction = [
+            event.project == @issue.project,
+            event.target_id == @issue.id,
+            event.target_type == @issue.class.name,
+            event.author_id == @members[0].id,
+            event.reopened? == true
+        ]
+        expect(condiction).to all( be true )
+      end
+
+    end
+  end
+end
