7e9c84a28a138d6ba7d64e72bcc7c519bea74776
jasonChiu <jason1122g@gmail.com>
2015-07-05 16:45:14 +0800

    fix  project operations

--- spec/operations/project_operation/create_spec.rb
+++ spec/operations/project_operation/create_spec.rb
@@ -3,24 +3,31 @@ require 'rails_helper'
 RSpec.describe ProjectOperation::Create do
 
   describe '#process' do
+
     context 'when given valid params' do
+
       before(:context) do
         @params = new_param({ project:{
-                                name: 'test'
-                            }})
+            name: 'test'
+        }})
         @user = FactoryGirl.create(:user)
 
         @operation = ProjectOperation::Create.new(@user)
         @operation.process(@params)
       end
 
-      it 'create a project with a given name' do
-        condiction = [
+      it 'creates a record in database' do
+        expect( @operation.model.persisted? ).to be true
+      end
+
+      it 'creates a project with a given name' do
+        conditions = [
             @operation.model.name == 'test',
             @operation.model.has_member?(@user) == true
         ]
-        expect(condiction).to all( be true )
+        expect( conditions ).to all( be true )
       end
+
     end
 
     context 'when given params without project' do
@@ -28,5 +35,6 @@ RSpec.describe ProjectOperation::Create do
         expect{ ProjectOperation::Create.new(@user).process(new_param) }.to raise_error ActionController::ParameterMissing
       end
     end
+
   end
 end
--- spec/operations/project_operation/update_spec.rb
+++ spec/operations/project_operation/update_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ProjectOperation::Update do
+
   include_context 'project with members'
 
   subject { ProjectOperation::Update.new(@members[0], @project) }
 
   describe '#process' do
-    context 'when given valid params' do
 
-      it 'change the name' do
-        subject.process(new_param({ project: {name: 'NewName'} }))
+    context 'when given valid params' do
+      it 'changes the name' do
+        subject.process(new_param({ project: { name: 'NewName' } }))
         expect(@project.name).to eq 'NewName'
       end
-
     end
 
     context 'when given params without project' do
@@ -20,5 +20,6 @@ RSpec.describe ProjectOperation::Update do
         expect{subject.process(new_param)}.to raise_error ActionController::ParameterMissing
       end
     end
+
   end
 end
