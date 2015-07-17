db4be570af9335cdc75f4c8b977acd75ed3559e8
jasonChiu <jason1122g@gmail.com>
2015-07-18 02:35:43 +0800

    add issue api

---
+++ app/api/v1/issue_api.rb
@@ -0,0 +1,20 @@
+module V1
+  class IssueAPI < Grape::API
+
+    resources :issues do
+
+      route_param :id do
+        before do
+          @issue ||= @project.issues.includes( participations: :user ).where( :sequential_id => params[:id] ).first
+        end
+
+        desc 'issue show'
+        get do
+          present @issue, with: Issues::IssueEntity
+        end
+      end
+
+    end
+
+  end
+end
