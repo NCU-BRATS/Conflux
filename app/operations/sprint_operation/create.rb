09a0d4a283150dd1a3a050d800a398d2eaa79b0b
Rueian Oneecom <rueiancsie@gmail.com>
2015-08-22 00:38:44 +0800

    init statuses field of sprint

--- app/operations/sprint_operation/create.rb
+++ app/operations/sprint_operation/create.rb
@@ -12,6 +12,7 @@ module SprintOperation
       if validate(sprint_params.except(:issue_ids)) && sync
         @model.user   = @current_user
         @model.project = @project
+        @model.statuses = [{id: 1,name: 'backlog'}, {id: 2,name: 'done'}]
         if @model.save
           @model.update_attributes(issue_ids: sprint_params[:issue_ids])
           ParticipationOperation::Create.new(@current_user, @model).process
