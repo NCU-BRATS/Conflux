e7f97ff80a2aa4585edcb0ccb34c6a7f8ee9e7fa
jasonChiu <jason1122g@gmail.com>
2015-08-24 15:26:09 +0800

    remove comment field of poll create operation

--- app/operations/poll_operation/create.rb
+++ app/operations/poll_operation/create.rb
@@ -1,7 +1,5 @@
 module PollOperation
   class Create < BaseForm
-    property :content, virtual: true
-    validates :content, presence: true
 
     def initialize(current_user, project)
       @current_user = current_user
@@ -21,9 +19,6 @@ module PollOperation
         @model.user    = @current_user
         @model.project = @project
         if @model.save
-          comment_param = ActionController::Parameters.new({comment: {content: params[:content]}})
-          CommentOperation::Create.new(@current_user, @model).process(comment_param)
-
           ParticipationOperation::Create.new(@current_user, @model).process
           BroadcastService.fire(:on_poll_created, @model, @current_user)
         end
