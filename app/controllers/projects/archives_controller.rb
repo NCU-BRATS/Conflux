bf2ca5019bc2a549e576c9d2dce48340c70b3fbe
jasonChiu <jason1122g@gmail.com>
2015-08-26 16:30:21 +0800

    add channel archive view

--- app/controllers/projects/archives_controller.rb
+++ app/controllers/projects/archives_controller.rb
@@ -2,6 +2,7 @@ class Projects::ArchivesController < Projects::ApplicationController
 
   def index
     @private_pub_channel1 = "/projects/#{@project.id}/sprints"
+    @private_pub_channel2 = "/projects/#{@project.id}/channels"
     respond_with @project
   end
 
