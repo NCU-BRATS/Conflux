b32937a642cd1c8692b85c4d66a82bd090241ba8
BDSword <thetruebloodsword@gmail.com>
2015-09-05 19:28:39 +0800

    modify attachment and sprint mention flag

--- spec/services/sprint_mention_service_spec.rb
+++ spec/services/sprint_mention_service_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe SprintMentionService do
         @sprint_mention_service = SprintMentionService.new
       end
 
-      it 'replaces pattern !SPRINT_ID with a link to the sprint' do
-        link_0 = "<a href=\"#{project_sprint_path(@project, @sprints[0])}\" class=\"sprint-mention\">!#{@sprints[0].sequential_id}</a>"
-        link_1 = "<a href=\"#{project_sprint_path(@project, @sprints[1])}\" class=\"sprint-mention\">!#{@sprints[1].sequential_id}</a>"
+      it 'replaces pattern ##SPRINT_ID with a link to the sprint' do
+        link_0 = "<a href=\"#{project_sprint_path(@project, @sprints[0])}\" class=\"sprint-mention\">###{@sprints[0].sequential_id}</a>"
+        link_1 = "<a href=\"#{project_sprint_path(@project, @sprints[1])}\" class=\"sprint-mention\">###{@sprints[1].sequential_id}</a>"
 
         parse_result = @sprint_mention_service.parse_mention(
-            "This is a test comment which references !#{@sprints[0].sequential_id} and !#{@sprints[1].sequential_id}",
+            "This is a test comment which references ###{@sprints[0].sequential_id} and ###{@sprints[1].sequential_id}",
             @project
         )
         expect(parse_result[:filtered_content]).to eq(
@@ -25,22 +25,22 @@ RSpec.describe SprintMentionService do
         )
       end
 
-      it 'would not replaced pattern !SPRINT_ID with a link if sprint is not exist' do
-        paragraph = "This is a test comment which references !NOT_EXIST_SPRINT."
+      it 'would not replaced pattern ##SPRINT_ID with a link if sprint is not exist' do
+        paragraph = "This is a test comment which references ##NOT_EXIST_SPRINT."
         parse_result = @sprint_mention_service.parse_mention(paragraph, @project)
         expect(parse_result[:filtered_content]).to eq(paragraph)
       end
 
       it 'would return the sprints it parsed out' do
         parse_result = @sprint_mention_service.parse_mention(
-            "This is a test comment which references !#{@sprints[0].sequential_id} and !#{@sprints[1].sequential_id}",
+            "This is a test comment which references ###{@sprints[0].sequential_id} and ###{@sprints[1].sequential_id}",
             @project
         )
         expect(parse_result[:mentioned_sprints]).to eq([ @sprints[0], @sprints[1] ])
       end
 
       it 'would not return the sprints if sprints are not exist' do
-        paragraph = "This is a test comment which references !NOT_EXIST_SPRINT."
+        paragraph = "This is a test comment which references ##NOT_EXIST_SPRINT."
         parse_result = @sprint_mention_service.parse_mention(paragraph, @project)
         expect(parse_result[:mentioned_sprints]).to eq([])
       end
