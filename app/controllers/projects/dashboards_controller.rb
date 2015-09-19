e4150511a1eb5af48ff299372c98ed0ef018b78e
Rueian Oneecom <rueiancsie@gmail.com>
2015-09-19 23:15:25 +0800

    refine project dashboard

--- app/controllers/projects/dashboards_controller.rb
+++ app/controllers/projects/dashboards_controller.rb
@@ -3,7 +3,7 @@ class Projects::DashboardsController < Projects::ApplicationController
   def show
     @polls   = @project.polls.includes(:user,:options).open.order('id DESC').limit(10)
     @sprints = @project.sprints.includes(:issues).where( archived: false ).where('issues_count > 0').order('id DESC').limit(10)
-    @issues  = @project.issues.includes(:user).joins(:participations)
+    @issues  = @project.issues.includes(:user, :assignee, :labels).joins(:participations)
                              .where(participations: { user_id: current_user.id })
                              .where.not( status: '2' ).order('id DESC').limit(30)
     respond_with @project
