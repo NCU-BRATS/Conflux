55a0968eceff5c28575c5d5fd37167dce7b11fbe
JasonChiu <jason1122g@gmail.com>
2015-09-27 01:16:07 +0800

    update archives layout

--- app/controllers/projects/archives_controller.rb
+++ app/controllers/projects/archives_controller.rb
@@ -1,5 +1,9 @@
 class Projects::ArchivesController < Projects::ApplicationController
 
+  def determine_layout
+    request.format.html? ? 'project_settings' : 'application'
+  end
+
   def index
     @private_pub_channel1 = "/projects/#{@project.id}/sprints"
     @private_pub_channel2 = "/projects/#{@project.id}/channels"
