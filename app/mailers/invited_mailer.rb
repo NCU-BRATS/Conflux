12d13936fa3b9d2b4f3961eb6d55429390f74450
JasonChiu <jason1122g@gmail.com>
2015-10-04 16:14:02 +0800

    add pending member mailer

---
+++ app/mailers/invited_mailer.rb
@@ -0,0 +1,16 @@
+class InvitedMailer < ApplicationMailer
+
+  def invite_user(email,project,inviter)
+    @project = project
+    @inviter = inviter
+    mail(to: email, subject: "您已被邀請加入 #{project.name} 專案")
+  end
+
+  def join_user(user,project,inviter)
+    @user = user
+    @project = project
+    @inviter = inviter
+    mail(to: @user.email, subject: "您已被邀請加入 #{project.name} 專案")
+  end
+
+end
