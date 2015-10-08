a8e4b12764cbf26c06d5bcd7982fe02642445102
Rueian Oneecom <rueiancsie@gmail.com>
2015-10-08 14:33:04 +0800

    Auto fill email of invited user

--- app/mailers/invited_mailer.rb
+++ app/mailers/invited_mailer.rb
@@ -1,6 +1,7 @@
 class InvitedMailer < ApplicationMailer
 
   def invite_user(email, project, inviter)
+    @email = email
     @project = project
     @inviter = inviter
     mail(to: email, subject: "您已被邀請加入 #{project.name} 專案")
